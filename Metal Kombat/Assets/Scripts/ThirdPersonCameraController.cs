0d471df08b00eb8347d6c55af00a8b88e2a36c5f
marc <marcscolaire@gmail.com>
2019-08-29 17:03:14 -0400

    essaie de faire disparaitre le mur quand la caméra fonce dedans

--- Metal Kombat/Assets/Scripts/ThirdPersonCameraController.cs
+++ Metal Kombat/Assets/Scripts/ThirdPersonCameraController.cs
@@ -2,20 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonCameraController : MonoBehaviour {
+public class ThirdPersonCameraController : MonoBehaviour
+{
     public float RotationSpeed = 1;
     public Transform Target, Player;
     float mouseX, mouseY;
-	// Use this for initialization
-	void Start () {
+    public Transform Obstruction;
+    float zoomSpeed = 2f;
+    // Use this for initialization
+    void Start()
+    {
+        Obstruction = Target;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         CamControl();
-	}
+        ViewObstructed();
+    }
     void CamControl()
     {
         mouseX += Input.GetAxis("Mouse X") * RotationSpeed;
@@ -24,7 +31,42 @@ public class ThirdPersonCameraController : MonoBehaviour {
 
         transform.LookAt(Target);
 
-        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
-        Player.rotation= Quaternion.Euler(0, mouseX, 0);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        }
+        else
+        {
+
+            Player.rotation = Quaternion.Euler(0, mouseX, 0);
+        }
+
+    }
+
+    void ViewObstructed()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Target.position - transform.position, out hit, 4.5f))
+        {
+            if (hit.collider.gameObject.tag != "Player")
+            {
+                Obstruction = hit.transform;
+                Obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+
+                if (Vector3.Distance(Obstruction.position, transform.position) >= 3f && Vector3.Distance(transform.position, Target.position) >= 1.5f)
+                {
+                    transform.Translate(Vector3.forward * zoomSpeed * Time.deltaTime);
+                }
+            }
+            else
+            {
+                Obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                if ( Vector3.Distance(transform.position, Target.position) < 4.5f)
+                {
+                    transform.Translate(Vector3.back * zoomSpeed * Time.deltaTime);
+                }
+            }
+        }
     }
 }
