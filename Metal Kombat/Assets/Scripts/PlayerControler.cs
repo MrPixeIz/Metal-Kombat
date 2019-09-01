eb09c0343654c325cd99d323302972a9618f1203
MrPixelz <marcscolaire@gmail.com>
2019-09-01 18:08:35 -0400

    modification mouvements du personnage

--- Metal Kombat/Assets/Scripts/PlayerControler.cs
+++ Metal Kombat/Assets/Scripts/PlayerControler.cs
@@ -8,6 +8,8 @@ public class PlayerControler : MonoBehaviour
     public Animator animator;
     float InputX;
     public float InputY;
+    public float speed = 10.0F;
+    public float rotationSpeed = 10.0F;
 
     // Use this for initialization
     void Start()
@@ -18,9 +20,28 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        InputY = Input.GetAxis("Vertical");
-        InputX = Input.GetAxis("Horizontal");
-        animator.SetFloat("InputY", InputY);
-        animator.SetFloat("InputX", InputX);
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("isJumping");
+        }
+
+        if (translation != 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        
     }
 }
