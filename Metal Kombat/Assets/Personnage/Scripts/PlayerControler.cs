62eb661b349767ff95c65f59e0f9a77c7d1d1ed8
B533P-09\533 <aaaa@aaaa.com>
2019-09-23 17:07:53 -0400

    modification mouvement beta

--- Metal Kombat/Assets/Personnage/Scripts/PlayerControler.cs
+++ Metal Kombat/Assets/Personnage/Scripts/PlayerControler.cs
@@ -15,18 +15,22 @@ public class PlayerControler : MonoBehaviour
     private CharacterController controller;
     public bool isGrounded;
     private float gravity = 65.0f;
-    private float jumpForce = 20.0f;
+    private float jumpForce = 50.0f;
     private bool isCrouched = false;
     private float crouchStartTime;
     private bool isCrouching = false;
     private Sounds sounds;
     private Vector3 hitNormal;
-    private float fireDelay = 1f;
+    private float fireDelay = 0.5f;
     private float delayBeforeNextFire = 0;
     public GameObject raycastObject;
     private float verticalVel = 0;
     Vector3 fwd;
-
+    private bool canMove = true;
+    private int walkForce = 500;
+    private Vector3 moveVector;
+    private float slopeLimit = 80;
+    private float slideFriction = 0f;
 
     void Start()
     {
@@ -38,43 +42,68 @@ public class PlayerControler : MonoBehaviour
 
     void Update()
     {
-        Vector3 moveVector;
-        inputMagnitude();
+        moveVector.y = verticalVel;
+
+        InputMagnitude();
         isGrounded = GroundCheck();
-        moveVector = new Vector3(0, verticalVel, 0);
+
         if (isGrounded)
         {
             anim.SetBool("isFalling", false);
+            delayBeforeNextFire -= Time.deltaTime;
             if (Input.GetButtonDown("Jump") && isCrouched == false)
             {
+
                 Jump();
             }
             if (Input.GetKeyDown(KeyCode.C))
             {
+
                 Crouch();
             }
             if (Input.GetAxis("Fire1") != 0)
             {
                 Attack();
             }
-            //Test nouvelle méthode de déplacement
-            /*if (Input.GetAxis("Vertical") != 0)
-            {
-                moveVector += transform.forward * 5 * Time.deltaTime;
-            }*/
-
+            MoveCharacter();
         }
         else
         {
             anim.SetBool("isFalling", true);
-            float slideFriction = 0f;
-            moveVector.x = (((1f - hitNormal.y) * hitNormal.x * (1f - slideFriction)) * 10);
-            moveVector.z = (((1f - hitNormal.y) * hitNormal.z * (1f - slideFriction)) * 10);
-
+          
+            Slide();
         }
 
         verticalVel -= gravity * Time.deltaTime;
         controller.Move(moveVector * Time.deltaTime);
+    }
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        hitNormal = hit.normal;
+    }
+    void Slide()
+    {
+        if (Vector3.Angle(Vector3.up, hitNormal) <= slopeLimit)
+        {
+            moveVector.x = (((1f - hitNormal.y) * hitNormal.x * (1f - slideFriction)) * 10);
+            moveVector.z = (((1f - hitNormal.y) * hitNormal.z * (1f - slideFriction)) * 10);
+        }
+    }
+    void MoveCharacter()
+    {
+        if (canMove == true)
+        {
+            moveVector.x = 0;
+            moveVector.z = 0;
+            if (Input.GetAxis("Vertical") != 0)
+            {
+                moveVector += transform.forward * walkForce * Time.deltaTime * Mathf.Abs(Input.GetAxis("Vertical"));
+            }
+            if (Input.GetAxis("Horizontal") != 0)
+            {
+                moveVector += transform.forward * walkForce * Time.deltaTime * Mathf.Abs(Input.GetAxis("Horizontal"));
+            }
+        }
     }
     void Jump()
     {
@@ -83,32 +112,43 @@ public class PlayerControler : MonoBehaviour
     }
     void Crouch()
     {
-        if (!isCrouching)
+        if (canMove == true)
         {
-            isCrouching = true;
-            if (isCrouched)
-            {
-                isCrouched = false;
-            }
-            else
-            {
-                isCrouched = true;
-            }
-            anim.SetBool("isCrouched", isCrouched);
-            float crouchStartTime = Time.time;
-            if (isCrouched)
-            {
-                StartCoroutine(UpdateHeight(17, 10, 0.5f));
-            }
-            else
-            {
-                StartCoroutine(UpdateHeight(10, 17, 0.1f));
-            }
+            canMove = false;
         }
+        else
+        {
+            canMove = true;
+        }
+        //if (!isCrouching)
+        //{
+        print("Crouch Function");
+        //isCrouching = true;
+        if (isCrouched)
+        {
+            isCrouched = false;
+        }
+        else
+        {
+            isCrouched = true;
+        }
+        anim.SetBool("isCrouched", isCrouched);
+        float crouchStartTime = Time.time;
+        if (isCrouched)
+        {
+            print("crouch");
+            //StartCoroutine(UpdateHeight(17, 10, 0.5f));
+        }
+        else
+        {
+            // StartCoroutine(UpdateHeight(10, 17, 0.1f));
+            print("uncrouch");
+        }
+        //}
     }
     void Attack()
     {
-        delayBeforeNextFire -= Time.deltaTime;
+
         if (delayBeforeNextFire <= 0)
         {
             fwd = raycastObject.transform.TransformDirection(Vector3.forward);
@@ -129,22 +169,22 @@ public class PlayerControler : MonoBehaviour
         foward.Normalize();
         right.Normalize();
 
-         Vector3 desiredMoveDirection = foward * inputZ + right * inputX;      
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), desiredRotationSpeed);      
+        Vector3 desiredMoveDirection = foward * inputZ + right * inputX;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), desiredRotationSpeed);
     }
 
-    void inputMagnitude()
+    void InputMagnitude()
     {
         inputX = Input.GetAxis("Horizontal");
         inputZ = Input.GetAxis("Vertical");
         anim.SetFloat("InputZ", inputZ, 0.0f, Time.deltaTime * 2f);
         anim.SetFloat("InputX", inputX, 0.0f, Time.deltaTime * 2f);
-       float speed = new Vector2(inputZ, inputX).sqrMagnitude;
+        float speed = new Vector2(inputZ, inputX).sqrMagnitude;
 
         //Déplacer le joueur     
         anim.SetFloat("InputMagnitude", speed, 0.0f, Time.deltaTime);
         if (speed > allowPlayerRotation)
-        {           
+        {
             PlayerMoveAndRotation();
         }
     }
@@ -173,10 +213,13 @@ public class PlayerControler : MonoBehaviour
     {
         bool hitDetected = false;
         RaycastHit objectHit;
-        Debug.DrawRay(raycastObject.transform.position, fwd * 7, Color.green, 2);
-
-        if (Physics.Raycast(raycastObject.transform.position, fwd, out objectHit, 7 ))
+        Debug.DrawRay(raycastObject.transform.position + new Vector3(0, 5, 0), fwd * 3, Color.green, 2);
+        //Physics.Raycast(raycastObject.transform.position, fwd, out objectHit, 7
+        //Physics.SphereCast(transform.position + new Vector3(0, controller.height / 2, 0), controller.height / 2, transform.forward, out objectHit, 10)
+        if (Physics.Raycast(raycastObject.transform.position + new Vector3(0, 5, 0), fwd, out objectHit, 3))
         {
+
+
             hitDetected = true;
         }
         else
@@ -189,12 +232,15 @@ public class PlayerControler : MonoBehaviour
 
     IEnumerator UpdateHeight(float startHeight, float endHeight, float time)
     {
+
         float delta = Time.time - crouchStartTime;
         while (delta < 1)
         {
+
             delta = Time.time - crouchStartTime;
             float percentCompletion = delta / time;
             controller.height = Mathf.Lerp(startHeight, endHeight, percentCompletion);
+            print(Mathf.Lerp(startHeight, endHeight, percentCompletion));
             Mathf.Lerp(17, 10, delta);
             yield return new WaitForSeconds(0.1f);
         }
@@ -210,14 +256,12 @@ public class PlayerControler : MonoBehaviour
     }
     public void Punch(AudioClip clipAudio)
     {
-        if (isGrounded)
+
+        if (HitCheck())
         {
-            if (HitCheck())
-            {
-                sounds.PlaySound(clipAudio);
-                print("HIT");
-            }
+            sounds.PlaySound(clipAudio);
         }
+
     }
 
 }
