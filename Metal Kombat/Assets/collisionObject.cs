fce4111f9d0d7509e23b6ffd8c3593aebe210a82
asdf <asdf@hotmail.com>
2019-09-24 11:01:01 -0400

    map1 update

---
+++ Metal Kombat/Assets/collisionObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionObject : MonoBehaviour {
+
+    public int force = 1000;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        print("ON TRIGGER ENTER");
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name =="Player")
+        {
+            
+        }
+        
+    }
+}
