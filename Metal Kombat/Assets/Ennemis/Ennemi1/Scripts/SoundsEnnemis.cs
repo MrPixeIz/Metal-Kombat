f103da9d97ff2e62c61a39145b4f043f0a2dfb63
B533P-10\533 <mickaelbergeron14@gmail.com>
2019-09-24 08:43:49 -0400

    Ajout de l'ennemi (fonctionnel) :
    Fixer le Son de pas
    Start Point et End Point Ajouter

---
+++ Metal Kombat/Assets/Ennemis/Ennemi1/Scripts/SoundsEnnemis.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsEnnemis : MonoBehaviour {
+
+    public AudioClip audioClip;
+    public AudioSource audiosource;
+
+    void Start()
+    {
+        audiosource.clip = audioClip;
+
+    }
+
+    void Update()
+    {  
+    }
+
+    public void PlaySound(AudioClip clipAudio)
+    {
+        audiosource.clip = clipAudio;
+        audiosource.volume = Random.Range(0.4f, 0.5f);
+        audiosource.pitch = Random.Range(0.9f, 1.3f);
+        audiosource.Play();
+    }
+    
+}
