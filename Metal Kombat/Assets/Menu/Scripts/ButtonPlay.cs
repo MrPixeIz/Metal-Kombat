6545b0dd985a4d56f3c276578880d88fa2be9fd9
MrPixelz <marcscolaire@gmail.com>
2019-09-30 19:18:23 -0400

    Ajout du menu au jeu

---
+++ Metal Kombat/Assets/Menu/Scripts/ButtonPlay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonPlay : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void PlayLevelOne()
+    {
+        SceneManager.LoadScene("mapLevel1");
+    }
+    public void BackToMenu()
+    {
+ 
+        SceneManager.LoadScene("menu");
+    }
+    public void GoToAbout()
+    {
+        SceneManager.LoadScene("About");
+    }
+
+
+}
