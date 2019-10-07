cb5f4c0605feb1759b8550828bb5ed9953262036
MrPixelz <marcscolaire@gmail.com>
2019-10-07 06:37:27 -0400

    modification script personnage pour que le slide soit plus petit... le personnage fait des sauts de malade

--- Metal Kombat/Assets/Personnage/Scripts/PlayerControler.cs
+++ Metal Kombat/Assets/Personnage/Scripts/PlayerControler.cs
@@ -69,7 +69,7 @@ public class PlayerControler : MonoBehaviour
 
         if (hitNormal != Vector3.zero)
         {
-            float slopeLimit = 60;
+            float slopeLimit = 80;
             if (verticalVel < 0 && Vector3.Angle(Vector3.up, hitNormal) >= slopeLimit)
             {
                 float slideFriction = 0f;
