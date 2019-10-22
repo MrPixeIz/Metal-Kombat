3350f030d68d77db656f88c28fb7fa84b00cdd05
SkyBoy <luc.plante.guitare.skyboy69@gmail.com>
2019-10-22 11:52:17 -0400

    Create prefab switch begin script controler and add event at end of animation switch

---
+++ Metal Kombat/Assets/PowerSwitch/ControlerSwitch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlerSwitch : MonoBehaviour {
+
+    private Animator animation;
+    private AudioSource soundSwitch; 
+
+
+    // Use this for initialization
+    void Start () {
+
+        animation = this.GetComponentInChildren<Animator>();
+        soundSwitch = this.GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+    }
+
+    void OnTriggerEnter()
+    {
+        animation.SetTrigger("TriggerSwitch");
+        //soundSwitch.Play();
+
+
+    }
+
+    public void TurnLight()
+    {
+        print("cacou");
+        soundSwitch.Play();
+    }
+}
