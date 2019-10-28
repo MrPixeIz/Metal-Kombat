96ea9a036c030390b838ee1016678f899a4fdb70
SkyBoy69 <luc.plante.guitare.skyboy69@gmail.com>
2019-10-28 16:11:34 -0400

    Le prefab de la switch est fini la lumiere devient rouge et recommence la partie

---
+++ Metal Kombat/Assets/PowerSwitch/ChangeLightColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLightColor : MonoBehaviour {
+
+
+    public Renderer renderer;
+
+    // Use this for initialization
+    void Start () {
+        renderer = GetComponent<Renderer>();
+    }
+	
+    public void ChangeLightColorSwitch()
+    {
+        renderer.material.SetColor("_EmissionColor", new Color32(255, 0, 0, 255));
+        renderer.material.color = new Color32(255, 0, 0, 255);
+    }
+}
--- Metal Kombat/Assets/PowerSwitch/ControlerSwitch.cs
+++ Metal Kombat/Assets/PowerSwitch/ControlerSwitch.cs
@@ -1,37 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ControlerSwitch : MonoBehaviour {
 
     private Animator animation;
-    private AudioSource soundSwitch; 
-
+    private AudioSource electricSound;
+    private AudioSource soundSwitch;
+    private ChangeLightColor changelightColor;
+    private bool EndGame = false;
+    private float timeLeft = 5.0f;
 
     // Use this for initialization
     void Start () {
 
         animation = this.GetComponentInChildren<Animator>();
-        soundSwitch = this.GetComponent<AudioSource>();
-    }
-	
-	// Update is called once per frame
-	void Update () {
+        AudioSource[] audios = GetComponents<AudioSource>();
+        electricSound = audios[0];
+        soundSwitch = audios[1];
 
-        
+        changelightColor = GetComponentInChildren<ChangeLightColor>();
+
+    }
+
+    void Update()
+    {
+        if (EndGame == true)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                SceneManager.LoadScene(1);
+            }
+        }
+
     }
 
     void OnTriggerEnter()
     {
-        animation.SetTrigger("TriggerSwitch");
-        //soundSwitch.Play();
-
-
+       animation.SetTrigger("TriggerSwitch");      
+       soundSwitch.Play();
+      
     }
 
     public void TurnLight()
     {
-        print("cacou");
-        soundSwitch.Play();
+       electricSound.Play();
+       changelightColor.ChangeLightColorSwitch();
+       EndGame = true;
     }
+
 }
